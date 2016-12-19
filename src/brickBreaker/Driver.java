04e13afcc4ba89465e48cc08b26b08ea5c1ec33a
Anthony Corbin <dragon1672@gmail.com>
2016-12-18 19:16:10 -0800

    Driver class has battleAI ability...which currently freezes :/

--- src/brickBreaker/Driver.java
+++ src/brickBreaker/Driver.java
@@ -2,9 +2,8 @@ package brickBreaker;
 
 import brickBreaker.ai.AI;
 import brickBreaker.ai.QuickAI;
-import brickBreaker.board.BoardGenerator;
-import brickBreaker.board.CachedBoard;
-import brickBreaker.board.PlayableBoard;
+import brickBreaker.ai.QuickyBoardHuristic;
+import brickBreaker.board.*;
 import utils.IntVector2D;
 
 import java.io.IOException;
@@ -36,7 +35,7 @@ public class Driver {
     }
 
     private static String getStaticBoardString() {
-        //*
+        /*
         return "" +
                 "    Y    \n" +
                 "    Y    \n" +
@@ -63,7 +62,45 @@ public class Driver {
         //*/
     }
 
-    public static void main(String... args) throws IOException {
+    private static int getAIScore(ReadOnlyBoard board, AI ai) {
+        List<IntVector2D> winningMoves = ai.getWinningMoveSet(board);
+        PlayableBoard boardToGetScore = BoardDuplicator.duplicate(board);
+        return winningMoves.stream().map(boardToGetScore::popCell).reduce((o1,o2) -> o1 + o2).orElse(-1);
+    }
+
+    private static int battleAI(PlayableBoard board, AI ai1, AI ai2) {
+        int ai1Score = getAIScore(board,ai1);
+        int ai2Score = getAIScore(board,ai2);
+        int winner = 0;
+        if(ai1Score > ai2Score) winner = -1;
+        if(ai1Score < ai2Score) winner = 1;
+        String winnerStr = winner == 0 ? "tie" : winner > 0 ? "2" : "1";
+        System.out.printf("Winner: %s, score Ai1: %s, Ai2: %s\n",winnerStr,ai1Score,ai2Score);
+        return winner;
+    }
+
+    private static void battleAI(AI ai1, AI ai2, int rounds) {
+        int overallWinner = 0;
+        for (int i = 0; i < rounds; i++) {
+            PlayableBoard board = new BoardGenerator().generateRandomBoard(10,10).getAsPlayableBoard();
+            overallWinner += battleAI(board,ai1,ai2);
+        }
+        String overallWinnerString =  overallWinner == 0 ? "tie" : overallWinner > 0 ? "2" : "1";
+        System.out.println("Overall Winner");
+    }
+
+    private static void battleAI_main() throws IOException {
+        AI ai1 = new QuickAI();
+        AI ai2 = new QuickyBoardHuristic();
+
+        //PlayableBoard board = new BoardGenerator().generateFromString(getStaticBoardString()).getAsPlayableBoard();
+        //PlayableBoard board = new BoardGenerator().generateFromImage("Capture.png",10,10).getAsPlayableBoard();
+
+        //battleAI(board, ai1,ai2);
+        battleAI(ai1,ai2,5);
+    }
+
+    public static void mainTester() throws IOException {
         AI ai = new QuickAI();
 
         BoardGenerator boardGenerator = new BoardGenerator();
@@ -75,5 +112,10 @@ public class Driver {
         PlayableBoard playableBoard = new CachedBoard(boardGenerator.getAsPlayableBoard());
 
         playAIGame(playableBoard, ai);
+    }
+
+    public static void main(String... args) throws IOException {
+        //mainTester();
+        battleAI_main();
     }
 }
